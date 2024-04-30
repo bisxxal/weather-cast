5e3a02eebad53f31046a9ee34673305b23f6450c
unknown <bishalkandi859494@gmail.com>
2024-05-01 00:03:53 +0530

    fist comit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -13,72 +13,85 @@ import snow from '../assets/snow.png'
 import scatter from '../assets/scatter.png'
 import thunder from '../assets/thunder.png'
 import search from '../assets/search.png'
+
+const fewcloudbg = `https://plus.unsplash.com/premium_photo-1667143327560-ac86c2b8afa6?q=80&w=2040&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D`
+const mistbggg = `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSy6RdFnWLMtxoRLM9OiMaxz5bw05ongU5Je3-gznsBTg&s`
+const rainbg =`https://images.unsplash.com/photo-1438449805896-28a666819a20?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D`
+const snowbg =`https://plus.unsplash.com/premium_photo-1675715924010-11ffef953747?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D`
+const cloudybgg= `https://images.unsplash.com/photo-1641933002369-1122e78d0b47?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D`
+const clearbg = `https://images.unsplash.com/photo-1419833173245-f59e1b93f9ee?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D`
+const thunderbg  = `https://images.unsplash.com/photo-1465799522714-8eb0e6fccf73?q=80&w=1913&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D`
 function Home() {
 
     const [data , setdata] = useState(null);
     const [input ,setinput] =useState('bhubaneswar')
     const [wicon ,setWicon] = useState()
+    const [bgwicon ,setBgWicon] = useState( )
    
     
     const featchdata = async ()=>{
         const url = `https://api.openweathermap.org/data/2.5/weather?q=${input}&units=Metric&appid=${api_key}`
         await fetch(url).then(res=>res.json()).then(data=>setdata(data));
-        
     }  
 useEffect(()=>{
     if(input !== undefined  ){
-        
         featchdata()
     }  
 
 if(data !== null){
-
-
     if(data.weather[0].icon === '01d' ||data.weather[0].icon === '01n' ){
         setWicon(clear);
+        setBgWicon(clearbg)
         }
-    
     if(data.weather[0].icon === '02d' ||data.weather[0].icon === '02n' ){
         setWicon(fewcloud);
+        setBgWicon(fewcloudbg)
         }
     
     if(data.weather[0].icon === '03d' ||data.weather[0].icon === '03n' ){
         setWicon(scatter);
+        setBgWicon(clearbg)
         }
     
     if(data.weather[0].icon === '04d' ||data.weather[0].icon === '04n' ){
         setWicon(cloudy);
+        setBgWicon(cloudybgg)
         }
     
     if(data.weather[0].icon === '09d' ||data.weather[0].icon === '09n' ){
         setWicon(rain);
+        setBgWicon(rainbg)
         }
     
     if(data.weather[0].icon === '10d' ||data.weather[0].icon === '10n' ){
         setWicon(rain);
+        setBgWicon(rainbg)
         }
     
     if(data.weather[0].icon === '11d' ||data.weather[0].icon === '11n' ){
         setWicon(thunder);
+        setBgWicon(thunderbg)
         }
     
     if(data.weather[0].icon === '13d' ||data.weather[0].icon === '13n' ){
         setWicon(snow);
+        setBgWicon(snowbg)
         }
     
     if(data.weather[0].icon === '50d' ||data.weather[0].icon === '50n' ){
         setWicon(mist);
+        setBgWicon(mistbggg)
         }
     } 
 },[  input,featchdata  ])
  
 return (
-    <div className='w-full h-screen bg-[#295a99cf] flex flex-col overflow-hidden text-white justify-between py-5 bg-[url("https://images.unsplash.com/photo-1675640145657-7ac2289cb4bb?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")] bg-center bg-cover bg-no-repeat'>
+    <div className={`w-full h-screen bg-[#295a99cf] flex flex-col overflow-hidden text-white justify-between py-5   `} style={{ backgroundColor: "#295a99cf", backgroundImage: `url(${bgwicon})`, backgroundPosition: "center", backgroundSize: "cover", backgroundRepeat: "no-repeat" }}>
 
         <div className='px-5 mb-7 lg:mb-0  lg:px-9 items-center flex justify-between ' > 
-        <div> <h1 className='text-[25px] lg:text-[4vh]  text-white '>wpCAST</h1></div>
+        <div> <h1 className='text-[25px] lg:text-[4vh]  text-white '>Wcast</h1></div>
          
-        <div className='flex items-center bg-[#33619d44] border-[#ffffff84]  border-[0.1px] backdrop-blur-[9px] rounded-3xl px-3'> <input   onKeyDown={(e) => { 
+        <div className='flex items-center   border-[#ffffff84]  border-[0.1px] backdrop-blur-[9px] rounded-3xl px-3'> <input   onKeyDown={(e) => { 
                         if (e.key === "Enter") { 
                            setinput(e.target.value) 
                         } 
@@ -91,7 +104,7 @@ return (
 
         <div className='flex w-full flex-wrap-reverse px-[15%] gap-5 justify-center  lg:justify-between'>
            
-        <div className='temp lg:w-[330px] w-full border-[#ffffff50]  border-[0.1px] backdrop-blur-[9px] px-[10px] lg:px-[20px] lg:pr-[35px] rounded-2xl py-4 bg-[#33619d4e] h-[250px]  lg:h-[300px]'> 
+        <div className='temp lg:w-[330px] w-full border-[#ffffff50]  border-[0.1px] backdrop-blur-[9px] px-[10px] lg:px-[20px] lg:pr-[35px] rounded-2xl py-4   h-[250px]  lg:h-[300px]'> 
          
            <p>Current Weather </p> 
            
@@ -110,7 +123,7 @@ return (
         </div>
 
 
-        <div className='city backdrop-blur-[9px] py-2 lg:py-0 rounded-3xl w-full lg:w-[40%] flex items-center justify-center flex-col bg-[#33619d44] border-[#ffffff84]  border-[0.1px]'>
+        <div className='city backdrop-blur-[9px] py-2 lg:py-0 rounded-3xl w-full lg:w-[40%] flex items-center justify-center flex-col   border-[#ffffff84]  border-[0.1px]'>
             <p className=' text-start'>Currently showing...</p>
             <div className='flex items-center gap-5 justify-center'>
             <FiMapPin className=' text-[25px]' />
@@ -122,8 +135,10 @@ return (
 
         <div className='flex lg:gap-[unset] gap-4  w-full justify-evenly flex-wrap '>
             <div className='flex  flex-col gap-4 items-center justify-center  h-[150px] lg:h-[240px] w-[45%] lg:w-[300px] border-[#ffffff4b] rounded-2xl border-[0.1px] backdrop-blur-[20px]'> <p>Wind Speed</p> <div className='flex items-center gap-4'><BsWind className=' text-[30px]' /> <p className=' text-[25px] font-medium'>{data?data.wind.speed:'0'}</p>km/h </div></div>
-             <div className='flex flex-col gap-4 px-6  items-center justify-center h-[180px] lg:h-[240px] w-[45%] lg:w-[300px] border-[#ffffff4b] rounded-2xl border-[0.1px] backdrop-blur-[20px]'> <p>Visibility</p> <div className="flex items-center gap-4"><MdOutlineVisibility  className=' text-[30px]' /> <p className=' text-[25px] font-medium'>{data?data.visibility:'0'}</p></div> 
-           <div className='flex gap-6'>  <p className='text-[18px] '>Min Temp{data? Math.floor(data.main.temp_min):'0'}</p>
+
+
+             <div className='flex flex-col gap-1 lg:gap-4 px-6  items-center justify-center h-[150px] lg:h-[240px] w-[45%] lg:w-[300px] border-[#ffffff4b] rounded-2xl border-[0.1px] backdrop-blur-[20px]'> <p>Visibility</p> <div className="flex items-center gap-4"><MdOutlineVisibility  className=' text-[30px]' /> <p className=' text-[25px] font-medium'>{data?data.visibility:'0'}</p></div> 
+           <div className='flex flex-col lg:flex-row  gap-1'>  <p className='text-[18px] '>Min Temp{data? Math.floor(data.main.temp_min):'0'}</p>
           <p className=' text-[18px]'>Max Temp {data? Math.floor(data.main.temp_max):'0'}</p>
              </div>
                </div>
